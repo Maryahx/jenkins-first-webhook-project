c2bb920ded5db5b6c57ed955859d8c671c1b5bb2
Maryahx <158805298+Maryahx@users.noreply.github.com>
2024-04-24 10:25:39 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
   public static void main (String[] args){
     System.out.println("Hello world :D!");
     System.out.println("Hello again :)");
-    System.out.println("This should be triggered :)");
+    System.out.println("This should be triggered..");
   }
 }
